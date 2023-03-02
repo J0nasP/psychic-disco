414f1767a0bd44f200b9247516efb2d987459849
J0n4sP <petersen0108@gmail.com>
2023-03-02 08:49:37 +0100

    Added the rigth asset path to image

--- visible_history_app/lib/login_form.dart
+++ visible_history_app/lib/login_form.dart
@@ -18,36 +18,48 @@ class MyCustomFormState extends State<MyCustomForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
-              ),
-              validator: usernameValidation,
+      backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/png/logo-no-background.png'),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Username'),
+                  autofocus: true,
+                  validator: usernameValidation,
+                ),
+                SizedBox(
+                  height: 17,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                  validator: passwordValidation,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing...')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit')),
+              ],
             ),
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-              validator: passwordValidation,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing...')),
-                    );
-                  }
-                },
-                child: const Text('Submit')),
-          ],
+          ),
         ),
       ),
     );
